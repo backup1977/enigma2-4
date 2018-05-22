6999355cad151781ab07e221f18905bbfd7d2811
backup1977 <waelghani@yahoo.de>
2018-05-22 18:18:08 +0100

    Update tstools.cpp

--- lib/dvb/tstools.cpp
+++ lib/dvb/tstools.cpp
@@ -719,10 +719,14 @@ int eDVBTSTools::findFrame(off_t &_offset, size_t &len, int &direction, int fram
 			/* we know that we aren't recording startcode 0x09 for mpeg2, so this is safe */
 			/* TODO: check frame_types */
 		// is_frame
-		if (((data & 0xFF) == 0x0009) || ((data & 0xFF) == 0x00)) /* H.264 UAD or MPEG2 start code */
+		if (((data & 0xFF) == 0x0009) || ((data & 0xFF) == 0x00) || ((data & 0x7E) == 0x0046)) /* H.264 UAD or H.265 UAD or MPEG2 start code */
 		{
 			++nr_frames;
 			if ((data & 0xE0FF) == 0x0009)		/* H.264 NAL unit access delimiter with I-frame*/
+			{
+				break;
+			}
+			if ((data & 0xE07E) == 0x0046) 		/* H.265 NAL unit access delimiter with I-frame*/
 			{
 				break;
 			}
